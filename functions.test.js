be508385f9ff62cbd74ecc979f7e3133cd1d9f96
Gabriel Cordeiro <120519526+GabrielFRCordeiro@users.noreply.github.com>
2025-03-05 19:32:18 -0300

    studying AAA pattern

--- functions.test.js
+++ functions.test.js
@@ -1,5 +1,25 @@
 const functions= require('./functions');
 
 test('Add 2 + 2 to equal 4', () => {
-    expect(functions.add(2, 2)).toBe(4);
-})
+    // Arrange
+    const a = 2;
+    const b = 2;
+
+    // Act
+    const resultado = functions.add(a, b);
+
+    // Assert
+    expect(resultado).toBe(4);
+});
+
+test('Add 11 + 31 to equal 42', () => {
+    // Arrange
+    const a = 11;
+    const b = 31;
+
+    // Act
+    const resultado = functions.add(a, b);
+
+    // Assert
+    expect(resultado).toBe(42);
+});
